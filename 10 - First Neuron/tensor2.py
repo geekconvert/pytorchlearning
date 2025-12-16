da7aad6d0d6790fa73039e1038cb731eb5367dc0
anish kumar <kumaranishdubey@gmail.com>
2025-12-16 21:19:50 +0530

    init

--- 10 - First Neuron/tensor2.py
+++ 10 - First Neuron/tensor2.py
@@ -7,8 +7,11 @@ X1 = torch.tensor([10, 38, 100, 150])
 
 y_pred = 1 * b + X1 * w1
 
-# print(b.shape)
-# print(X1.shape)
-# print(b.size())
-# print(X1.size())
-print(y_pred[1].item())
+print(b.shape) # this means that this tensor here only holds a single value.
+print(X1.shape) # there are now here four elements in this tensor.
+print(b.size())
+print(X1.size())
+print(y_pred[0]) # to access the first element of the tensor, But this is still in the tensor.
+print(y_pred[1].item()) # to extract the value from the tensor.
+
+# So this shape property allows you to identify the structure of the data that is in a tensor. size() and shape do tha same thing, they are equivalent.
