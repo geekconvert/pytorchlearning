6d0fe2cb09c3507a66e2315c2adf447309e78398
anish kumar <kumaranishdubey@gmail.com>
2025-12-23 06:45:58 +0530

    init

--- 25 - Spam/neuron_evaluate.py
+++ 25 - Spam/neuron_evaluate.py
@@ -29,10 +29,26 @@ for i in range(10000):
 
 model.eval()
 with torch.no_grad():
-    y_pred = nn.functional.sigmoid(model(X)) > 0.5
+    #  let's say the specificity is not that important. Meaning it's okay if every now and then a non-spam message, uh, gets detected as spam, then it could be, for example, here I could change the percentage to 0.25 and everything where the model is more confident than 25% that it is spam, we will now consider as spam.
+    # y_pred = nn.functional.sigmoid(model(X)) > 0.5
+    y_pred = nn.functional.sigmoid(model(X)) > 0.25
     # print(y_pred)
     # print(y)
     # y = (y == 1) no need to convert y to boolean as without it also same result.
+
+    """
+    y_pred[y == 1] uses boolean indexing: it keeps only the entries in y_pred where the corresponding entry in y equals 1.
+
+    Example:
+
+    y_pred = tensor([0.2, 0.8, 0.6, 0.1])
+    y = tensor([0., 1., 1., 0.])
+    y == 1 → tensor([False, True, True, False])
+
+    y_pred[y == 1] → tensor([0.8, 0.6]) (only predictions for the positive class).
+    """
+
+    # We now have some measurements here that would allow us to actually tune th cutoff value 0.25 based on what we want to optimize for.
     print("accuracy:", (y_pred == y).type(torch.float32).mean())
     print("sensitivity:", (y_pred[y == 1] == y[y == 1]).type(torch.float32).mean())
     print("specificity:", (y_pred[y == 0] == y[y == 0]).type(torch.float32).mean())
