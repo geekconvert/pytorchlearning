ff33b22641edb3d11974557cedee8f134e71a52f
anish kumar <kumaranishdubey@gmail.com>
2025-12-27 16:11:48 +0530

    init

--- 30 - From Neuron to Network/network_batch2.py
+++ 30 - From Neuron to Network/network_batch2.py
@@ -1,22 +1,13 @@
-import sys
 import torch
 from torch import nn
 import pandas as pd
 
 df = pd.read_csv("./data/student_exam_data.csv")
 
-X = torch.tensor(
-    df[["Study Hours", "Previous Exam Score"]].values, 
-    dtype=torch.float32
-)
-y = torch.tensor(df["Pass/Fail"], dtype=torch.float32)\
-    .reshape((-1, 1))
+X = torch.tensor(df[["Study Hours", "Previous Exam Score"]].values, dtype=torch.float32)
+y = torch.tensor(df["Pass/Fail"], dtype=torch.float32).reshape((-1, 1))
 
-model = nn.Sequential(
-    nn.Linear(2, 10),
-    nn.ReLU(),
-    nn.Linear(10, 1)
-)
+model = nn.Sequential(nn.Linear(2, 10), nn.ReLU(), nn.Linear(10, 1))
 
 loss_fn = torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
@@ -24,12 +15,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 num_entries = X.size(0)
 batch_size = 32
 
-# print(X[32:64])
-#for start in range(0, num_entries, batch_size):
-#    end = min(num_entries, start + batch_size)
-#    print("start: ", start, "end: ", end)
-
-for i in range(0, 1000):
+for i in range(0, 20000):
     loss_sum = 0
     for start in range(0, num_entries, batch_size):
         end = min(num_entries, start + batch_size)
@@ -43,8 +29,10 @@ for i in range(0, 1000):
         loss_sum += loss.item()
         optimizer.step()
 
-    if i % 10 == 0:
+    
+    if i % 1000 == 0:
         print(loss_sum)
+
 
 model.eval()
 with torch.no_grad():
@@ -52,3 +40,58 @@ with torch.no_grad():
     y_pred = nn.functional.sigmoid(outputs) > 0.5
     y_pred_correct = y_pred.type(torch.float32) == y
     print(y_pred_correct.type(torch.float32).mean())
+
+# import sys
+# import torch
+# from torch import nn
+# import pandas as pd
+
+# df = pd.read_csv("./data/student_exam_data.csv")
+
+# X = torch.tensor(
+#     df[["Study Hours", "Previous Exam Score"]].values, 
+#     dtype=torch.float32
+# )
+# y = torch.tensor(df["Pass/Fail"], dtype=torch.float32)\
+#     .reshape((-1, 1))
+
+# model = nn.Sequential(
+#     nn.Linear(2, 10),
+#     nn.ReLU(),
+#     nn.Linear(10, 1)
+# )
+
+# loss_fn = torch.nn.BCEWithLogitsLoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
+
+# num_entries = X.size(0)
+# batch_size = 32
+
+# # print(X[32:64])
+# #for start in range(0, num_entries, batch_size):
+# #    end = min(num_entries, start + batch_size)
+# #    print("start: ", start, "end: ", end)
+
+# for i in range(0, 1000):
+#     loss_sum = 0
+#     for start in range(0, num_entries, batch_size):
+#         end = min(num_entries, start + batch_size)
+#         X_data = X[start:end]
+#         y_data = y[start:end]
+
+#         optimizer.zero_grad()
+#         outputs = model(X_data)
+#         loss = loss_fn(outputs, y_data)
+#         loss.backward()
+#         loss_sum += loss.item()
+#         optimizer.step()
+
+#     if i % 10 == 0:
+#         print(loss_sum)
+
+# model.eval()
+# with torch.no_grad():
+#     outputs = model(X)
+#     y_pred = nn.functional.sigmoid(outputs) > 0.5
+#     y_pred_correct = y_pred.type(torch.float32) == y
+#     print(y_pred_correct.type(torch.float32).mean())
