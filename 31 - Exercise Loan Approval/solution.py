0ca4c24b32ffcbe132aa5d996d94a86a0017a0e4
anish kumar <kumaranishdubey@gmail.com>
2025-12-27 17:48:01 +0530

    init

--- 31 - Exercise Loan Approval/solution.py
+++ 31 - Exercise Loan Approval/solution.py
@@ -1,4 +1,3 @@
-import sys
 import torch
 from torch import nn
 import pandas as pd
@@ -8,11 +7,27 @@ df = df[["loan_status", "person_income", "loan_intent", "loan_percent_income", "
 df = pd.get_dummies(df, columns=["loan_intent"])
 print(df.columns)
 
-y = torch.tensor(df["loan_status"], dtype=torch.float32)\
-    .reshape((-1, 1))
+y = torch.tensor(df["loan_status"], dtype=torch.float32).reshape((-1, 1))
 
-print(df.drop("loan_status", axis=1))
+# axis = 1 means we are dropping a column
 X_data = df.drop("loan_status", axis=1).astype('float32').values
-print(X_data.dtype)
 X = torch.tensor(X_data, dtype=torch.float32)
-print(X)
+
+# import sys
+# import torch
+# from torch import nn
+# import pandas as pd
+
+# df = pd.read_csv("data/loan_data.csv")
+# df = df[["loan_status", "person_income", "loan_intent", "loan_percent_income", "credit_score"]]
+# df = pd.get_dummies(df, columns=["loan_intent"])
+# print(df.columns)
+
+# y = torch.tensor(df["loan_status"], dtype=torch.float32)\
+#     .reshape((-1, 1))
+
+# print(df.drop("loan_status", axis=1))
+# X_data = df.drop("loan_status", axis=1).astype('float32').values
+# print(X_data.dtype)
+# X = torch.tensor(X_data, dtype=torch.float32)
+# print(X)
